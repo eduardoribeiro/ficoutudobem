34495702fa0a7d900c820d9297d3141d46507cdd
Eduardo Ribeiro <eduardo.ribeiro@outlook.pt>
2022-11-16 00:59:21 +0000

    Adjust logos background

--- src/components/logos.tsx
+++ src/components/logos.tsx
@@ -17,7 +17,7 @@ const PartnerLogo = ({
     alt = "",
   onClick
 }: PartnersLogosButton) => (
-  <Card>
+  <Card sx={{backgroundColor: 'rgba(255, 255, 255, 0.01)', boxShadow: 'none', transition: 'all .4s ease-in-out', '&:hover': { backgroundColor: 'rgba(255, 255, 255, 0.55)', boxShadow: '0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)'}}}>
     <CardActionArea sx={{ height: 100 }} title={alt} onClick={onClick}>
       <CardContent sx={{display: 'flex', maxHeight: 48, justifyContent: 'center', alignItems: 'center', padding: theme.spacing(2)}}>
         <img src={image} alt={alt} title={alt} style={{maxHeight: 100}} />
